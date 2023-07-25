c450dfce94cbc1d42e4ccdec1af7d521de6685b5
KitsuneUdon33 <140283206+KitsuneUdon33@users.noreply.github.com>
2023-07-25 09:20:48 +0900

    Add files via upload

---
+++ alert.js
@@ -0,0 +1,14 @@
+git commit -m "[Start]Webアプリケーションを作ってみよう-1"
+
+function loadHello(){
+  alert("Hello!");
+}
+
+function sum(){
+    let x = 3;
+    let y = 4;
+    let z = x + y;
+    alert(z);
+  }
+
+  git commit -m "[Finish]Webアプリケーションを作ってみよう-1"
